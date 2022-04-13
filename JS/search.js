ffb1caddec0e6e90b609e6d4a5905d00157099c2
KLUKLY <2603625960@qq.com>
2022-04-13 22:01:05 +0800

    修改查询功能

--- JS/search.js
+++ JS/search.js
@@ -1,3 +1,30 @@
+function search(){
+    $(".search_result").html("");
+    let search = $(".search_bar").val();
+    if(search=='') return;//如果搜索框没内容就不执行了
+    let reg = new RegExp(search);//获得正则匹配
+    
+    let page = 0;
+    while(list[page]){
+        if(list[page].title.match(reg)){
+            $(".search_result").append(
+                $('<li></li>').append(
+                    $('<a class="search_result_block" href="../articles/'+ list[page].href+'">' + '</a>').append(
+                        $('<div>'+ list[page].title + '</div>')
+                    ).append(
+                        $('<span>'+ list[page].time+'</span>')
+                    ).append(
+                        $('<span>'+ list[page].tag+'</span>')
+                    )
+                ).append(
+                    $('<hr>')
+                )
+            );
+        }
+        page++;
+    }
+}
+
 $(document).ready(()=>{
     $(".search_a").on("click",()=>{
         $(".search_mask").css("display","block");
@@ -28,15 +55,15 @@ $(document).ready(()=>{
         //     }
         // }
 
-        $.ajax({
-            url: '../feed.xml',
-            type: 'GET',
-            dataType: 'xml',
-            success:function(xml){
-                var article = $(xml).find('article').text();
-                console.log(article);
-            }
-        })
+        // $.ajax({
+        //     url: '../feed.xml',
+        //     type: 'GET',
+        //     dataType: 'xml',
+        //     success:function(xml){
+        //         var article = $(xml).find('article').text();
+        //         console.log(article);
+        //     }
+        // })
 
         // var xmlDoc = getxmlDoc("feed.xml");
         // var list = xmlDoc.getElementsByTagName("article");
@@ -44,56 +71,19 @@ $(document).ready(()=>{
         // while(list[num]){
         //     console.log(list[num].childNodes[0].nodeValue);
         // }
-
+        search();
     });
 
-    // function getxmlDoc(xmlFile) {
-    //     var xmlDoc;
-    //     if (window.ActiveXObject) {
-    //         xmlDoc = new ActiveXObject('Microsoft.XMLDOM');//IE
-    //         xmlDoc.async = false;
-    //         xmlDoc.load(xmlFile);
-    //     }
-    //     else if (isFirefox=navigator.userAgent.indexOf("Firefox")>0) { //火狐
-
-    //         xmlDoc = document.implementation.createDocument('', '', null);
-    //         xmlDoc.load(xmlFile);
-    //     }
-    //     else{ //谷歌
-    //       var xmlhttp = new window.XMLHttpRequest();
-    //         xmlhttp.open("GET",xmlFile,false);
-    //         xmlhttp.send(null);
-    //         if(xmlhttp.readyState == 4){
-    //         xmlDoc = xmlhttp.responseXML.documentElement;
-    //         } 
-    //     }
-
-    //     return xmlDoc;
-    // }
 })
-/*
-        $(".search_result").html("");
-        let search = $(".search_bar").val();
-        if(search=='') return;//如果搜索框没内容就不执行了
-        let reg = new RegExp(search);//获得正则匹配
-    
-        let page = 1;
-        let str = "#page" + page;  
-        // $("#search_tag").load('index.html '+str);
 
-        while($(str).length){        
-            // console.log($("search_tag"));
-            let e = $(str).children(".brief").children(".brief_title");//寻找文章链接
-            let res = e.text();
-            //正则匹配
-            if(res.match(reg)){
-                //嵌套添加
-                $(".search_result").append(
-                    $('<li></li>').append(e.clone())
-                );
-            }
-            page++;
-            str = "#page" + page;
-            // $("#search_tag").load('index.html '+str);
-        }
-*/
+var list = [{
+    title: '关于我写博客这件事',
+    tag: '随记',
+    time: '2022-3-19',
+    href: '1.html',
+},{
+    title: '陌生人的棺材',
+    tag: '写作',
+    time: '2022-4-6',
+    href: '2.html',
+}]
